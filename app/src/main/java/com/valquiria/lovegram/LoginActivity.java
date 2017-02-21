49e1fcdd9b0de0f8c83412129491dcaf01661ad6
Miguel Urquijo <miguelferu@gmail.com>
2017-02-21 17:14:05 -0500

    Initial commit

--- app/src/main/java/com/valquiria/lovegram/LoginActivity.java
+++ app/src/main/java/com/valquiria/lovegram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.valquiria.lovegram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount (View view) {
+
+
     }
 }
